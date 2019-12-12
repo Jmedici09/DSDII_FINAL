a7ec26e963f271114b7d26d517f5e258f0d0ece1
Jmedici09 <Jmedici09@gmail.com>
2019-12-12 15:50:18 -0500

    gitignore
    Maybe this is right

--- Tcl/synth.tcl
+++ Tcl/synth.tcl
@@ -15,73 +15,22 @@ set outputDir "../Reports"
 file mkdir $outputDir
 
 # Set up design sources and constraints
-read_verilog {
-	../Sources/divider.v
-	../Sources/control.v
-	../Sources/datapath.v
-	../Sources/lrShiftSFR.v
-	../Sources/lShiftSFR.v
-	../Sources/subSFR.v
-	../Sources/udCounterSFR.v}
-read_xdc $constraintsInputDir/constraints.xdc
+read_verilog $sourceInputDir/divider.v
+
 set_property part xc7a35tcpg236-2L [current_project]
 # ----------------------------------------------------- #
-##FSM Type: seq
-#
-##Step 3: Synth Design
-#synth_design -fsm_extraction sequential -directive AreaOptimized_high -resource_sharing on -retiming -top divider > $outputDir/synth_out_seq.rpt
-#
-## Write design checkpoint
-##good design to write a checkpoint after synth, implementation, and bitstream
-#write_checkpoint -force $outputDir/synth_checkpoint_seq.dcp
-#
-#opt_design -sweep -resynth_area -aggressive_remap -merge_equivalent_drivers -debug_log > $outputDir/opt_out_seq.rpt
-#
-## Report to csv file
-#report_utilization -file $outputDir/synth_utilization_seq.rpt
-#report_timing -file $outputDir/synth_timing_seq.rpt
-#
-##FSM Type: gray
-#
-##Step 3: Synth Design
-#synth_design -fsm_extraction gray -directive AreaOptimized_high -resource_sharing on -retiming -top divider > $outputDir/synth_out_gray.rpt
-#
-## Write design checkpoint
-##good design to write a checkpoint after synth, implementation, and bitstream
-#write_checkpoint -force $outputDir/synth_checkpoint_gray.dcp
-#
-#opt_design -sweep -resynth_area -aggressive_remap -merge_equivalent_drivers -debug_log > $outputDir/opt_out_gray.rpt
-#
-## Report to csv file
-#report_utilization -file $outputDir/synth_utilization_gray.rpt
-#report_timing -file $outputDir/synth_timing_gray.rpt
-
-##FSM Type: johnson
-
-##Step 3: Synth Design
-#synth_design -fsm_extraction johnson -directive AreaOptimized_high -resource_sharing on -retiming -top divider > $outputDir/synth_out_johnson.rpt
-#
-## Write design checkpoint
-##good design to write a checkpoint after synth, implementation, and bitstream
-#write_checkpoint -force $outputDir/synth_checkpoint_johnson.dcp
-#
-#opt_design -sweep -resynth_area -aggressive_remap -merge_equivalent_drivers -debug_log > $outputDir/opt_out_johnson.rpt
-#
-## Report to csv file
-#report_utilization -file $outputDir/synth_utilization_johnson.rpt
-#report_timing -file $outputDir/synth_timing_johnson.rpt
-
-#FSM Type: one-hot
+#FSM Type: Auto
 
 #Step 3: Synth Design
-synth_design -fsm_extraction one_hot -directive AreaOptimized_high -resource_sharing on -retiming -top divider > $outputDir/synth_out_one_hot.rpt
+synth_design -top divider > $outputDir/synth_out.rpt
 
 # Write design checkpoint
 #good design to write a checkpoint after synth, implementation, and bitstream
 #write_checkpoint -force $outputDir/synth_checkpoint_one_hot.dcp
 
-#opt_design -sweep -resynth_area -aggressive_remap -merge_equivalent_drivers -debug_log > $outputDir/opt_out_one_hot.rpt
+read_xdc $constraintsInputDir/constraints.xdc
 
 # Report to csv file
-report_utilization -file $outputDir/synth_utilization_one_hot.rpt
-report_timing -file $outputDir/synth_timing_one_hot.rpt
+report_utilization -file $outputDir/synth_utilization.rpt
+report_timing -file $outputDir/synth_timing.rpt
+
